3b7a8d75b97f96192a89e955cae70ec18cc1c6f8
Dayane <dls6@cin.ufpe.br>
2022-04-12 18:39:57 -0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :historics
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :exchange_rate do
+    get :latest_rate, on: :collection
+    get :historical_rates, on: :collection
+  end
 end
