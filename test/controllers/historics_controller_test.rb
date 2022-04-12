55f60a28abc99a5e67c0bdef745ec1bc9d11332f
Dayane <dls6@cin.ufpe.br>
2022-04-12 18:54:49 -0300

    create test: historic

---
+++ test/controllers/historics_controller_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class HistoricsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @historic = historics(:one)
+  end
+
+  test "should get index" do
+    get historics_url
+    assert_response :success
+  end
+
+  test "should create historic" do
+    assert_difference('Historic.count') do
+      post historics_url, params: { historic: { base: @historic.base, date: @historic.date, historical: @historic.historical, rates: @historic.rates, success: @historic.success, timestamp: @historic.timestamp } }
+    end
+
+    assert_response :created
+  end
+end
