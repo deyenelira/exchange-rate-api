803de38c18844a0eff46dd7541a3c933cb18dba6
Dayane <dls6@cin.ufpe.br>
2022-04-13 21:08:52 -0300

    add historic validates

--- app/models/historic.rb
+++ app/models/historic.rb
@@ -1,2 +1,8 @@
 class Historic < ApplicationRecord
+  validates :success, presence: true
+  validates :timestamp, presence: true
+  validates :base, presence: true
+  validates :date, presence: true
+  validates :rates, presence: true
+
 end
