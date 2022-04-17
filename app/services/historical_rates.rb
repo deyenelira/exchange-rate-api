ead684f4d8c1c7f3106783530a202fbc266540ce
Dayane <dls6@cin.ufpe.br>
2022-04-17 11:28:41 -0300

    Refactoring code

--- app/services/historical_rates.rb
+++ app/services/historical_rates.rb
@@ -32,7 +32,9 @@ class HistoricalRates
 
       request = Net::HTTP::Get.new(url)
       request['X-RapidAPI-Host'] = 'fixer-fixer-currency-v1.p.rapidapi.com'
-      request['X-RapidAPI-Key'] = '8829aa2d6amsh168397e64463c2fp1cdb0djsne545e1442df1'
+
+      #Inserir chave de acesso
+      #request['X-RapidAPI-Key'] = ''
 
       response = http.request(request)
       CreateHistoric.new(JSON.parse(response.read_body)).call
